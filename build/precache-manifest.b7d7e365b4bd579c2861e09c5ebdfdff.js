498acb14a72b00ffe23974b2d51090394cacd72e
Nicu Chiciuc <nicuchiciuc@gmail.com>
2019-01-17 18:06:55 +0200

    Fixed homepage.

---
+++ build/precache-manifest.b7d7e365b4bd579c2861e09c5ebdfdff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9b19d63815f9feae1f37",
+    "url": "./static/js/main.9b19d638.chunk.js"
+  },
+  {
+    "revision": "c024432f7d25dd050455",
+    "url": "./static/js/1.c024432f.chunk.js"
+  },
+  {
+    "revision": "9b19d63815f9feae1f37",
+    "url": "./static/css/main.4f69b1bb.chunk.css"
+  },
+  {
+    "revision": "c024432f7d25dd050455",
+    "url": "./static/css/1.1a8f2a02.chunk.css"
+  },
+  {
+    "revision": "d5e853cd651ebe9028d5ae4624204fd8",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48cf346dd43a4e51cbb8da399c4e33e4.js"
+  "./precache-manifest.b7d7e365b4bd579c2861e09c5ebdfdff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
